7fef19d8ae5d30a4f685476125634dfd0e5458e4
Rahul Gopinath <rahul@gopinath.org>
2018-07-10 15:43:30 +0200

    1

--- src/xchains.py
+++ src/xchains.py
@@ -6,6 +6,7 @@ import time
 import angr
 import random
 import claripy
+from ptpython.repl import embed
 
 # How to choose the next state. The Prob_Mul_Factor skews the
 # probability distribution of choise of elements from the list
@@ -13,7 +14,6 @@ import claripy
 # weightage to the later states. If it is 0, it is same as
 # random sampling.
 Prob_Mul_Factor = 0
-GC = False
 Closing_Threshold = 0.8
 Closing_Strip = True
 Closing_Buffer = 1000
@@ -34,7 +34,23 @@ def w(v):
 
 def log(v): w("\t%s\n" % v)
 
+def pop_last(arr):
+    i = arr.pop()
+    return i, arr
+
+def pop_random(arr):
+    i = arr.pop(random.randint(0, len(arr)-1))
+    return i, arr
+
 class Program:
+
+    def pop_smallest(self, arr):
+        res = sorted([(self.str_len(a[1]), i) for i,a in enumerate(arr)])
+        idx = res[0][1]
+        i = arr.pop(idx)
+        return i, arr
+        
+
     def __init__(self, exe):
         self.exe = exe
         # Use auto_load_libs = False to use symbolic summaries of libs
@@ -53,24 +69,20 @@ class Program:
         for i in range(Min_Input_Len):
             self.initial_state.add_constraints(self.arg1a[i] != 0)
 
+        self.state_constraints = {}
 
-        self.state = self.initial_state
-        self.update_constraint_rep(self.state)
+        self.last_char_checked = self.last_checked_idx(self.initial_state)
 
-        self.states = []
         self.extra_states = []
-
-        self.last_char_checked = 0
-        self.update_checked_idx()
 
     def is_successful(self, state):
         return self.success_fn == state.addr
 
-    def update_constraint_rep(self, state):
-        """
-        Used to check if a constraint has been updated
-        """
-        self.last_constraints = [claripy.simplify(c) for c in state.solver.constraints]
+    def get_constraint_range(self, state):
+        # Remember that str(state) is not a unique representation of
+        # state. A better cache key may be
+        # str(state.solver.constraints.simplify)
+        return [(state.solver.min(c), state.solver.max(c)) for c in self.arg1a]
 
     def update_args(self, input_len, prefix):
         """
@@ -81,7 +93,6 @@ class Program:
         """
         largs = range(0, input_len+1)
         arg1k = ['%s_%d' % (prefix, i) for i in largs]
-        self.arg1k8 = {i:'%s_%d_%d_8' % (prefix, i,i) for i in largs}
         self.arg1h = {k:claripy.BVS(k, 8) for k in arg1k}
         self.arg1h_ = {self.arg1h[k].args[0]:k for k in arg1k}
         self.arg1a = [self.arg1h[k] for k in arg1k]
@@ -92,7 +103,7 @@ class Program:
         while stk: stk, i = stk.next, i+1
         return i
 
-    def update_checked_idx(self):
+    def last_checked_idx(self, state):
         """
         Update the last character that has been resolved (i.e. made printable)
         """
@@ -107,30 +118,21 @@ class Program:
 
         # First generate the set of character constraints.
         db = set(reduce(lambda x, y: x.union(y),
-                         self.retrieve_vars_in_constraints(self.state)))
-        self.last_char_checked = 0
+                         self.retrieve_vars_in_constraints(state)))
         for i in Count_Down:
-            # If no constraints exist for this character, then this character
-            # has not been resolved yet.
-            if self.arg1k8[i] not in db: continue
             # if the current character checked is not printable, then
             # it stands to reason that it was not fully resolved. In which
             # case, continue to the next loop.
-            if not self.is_printable(self.arg1a[i]): continue
+            if not self.is_printable(state, i): continue
             # this is the first character to have been resolved fully.
-            self.last_char_checked = i
             return i
 
     def retrieve_vars_in_constraints(self, state):
         return [state.solver.variables(c) for c in state.solver.constraints]
 
-    def is_printable(self, char):
-        m, n = self.state.solver.min(char), self.state.solver.max(char)
+    def is_printable(self, state, i):
+        m, n = self.range_at(state, i)
         return m > 31 and n < 128
-
-    def make_printable(self, char):
-        self.initial_state.add_constraints(char < 128)
-        self.initial_state.add_constraints(char > 31)
 
     def get_fn_addr(self, fname):
         if 'cfg' not in self.__dict__:
@@ -140,21 +142,13 @@ class Program:
         assert len(found) == 1, "No address found for function : %s" % fname
         return found[0]
 
-    def begin_closing(self):
-        return self.last_char_checked > (Closing_Threshold * Max_Input_Len)
-
     def str_len(self, state):
         """
         Return the zero termination of the argument string
         """
         ret = 0
-        for i in Count_Down:
-            # we count from the back. So if any character
-            # does not have zero as a solution, then its
-            # next character is the zero termination
-            if not state.solver.solution(self.arg1a[i], 0):
-                ret = i + 1
-                break
+        for i in range(Max_Input_Len+1):
+            if state.solver.max(self.arg1a[i]) == 0: return i
         return ret
 
     def range_at(self, state, at):
@@ -162,188 +156,90 @@ class Program:
         return (state.solver.min(c), state.solver.max(c))
 
     def choose_a_successor_state(self, states):
-        """
-        Which successor state to expand? We may apply various heuristics here
-        First, we sort by the smallest strings (zero starting). Then choose
-        the smallest string(s)
-        -- another is to look for the stack depth. If we are above the threshold
-        (say 80% of Max_Input_Len), and we wish to start closing, then
-        we might choose the successor state that has the least self.stack_depth
-        On the other hand, it may be that closing requires additional procedures
-        in which case this heuristic might fail
-        -- Similarly, another alternative is to look at the constraints added on
-        the last character on each state. If the constraint on a state is
-        similar enough to the constraints on last_character - 1, then choose
-        the other.
-
-        Finally, we need to find a way to ignore the spurious constraints -- i.e
-        the constraints generated by strlen.
-        """
-        assert states
-        self.update_checked_idx()
-        last = self.last_char_checked
-        (m, n) = self.range_at(self.state, last)
-        ss = list(enumerate(states))
-        si = [i for i,s in ss]
-
-        # TODO: Explore the next n successors, and find if any of these have
-        # smaller stack depth.
-        if self.begin_closing():
-            # begin closing -- stack depth gets priority
-            ss = [(self.stack_depth(s), i) for i, s in ss]
-            di = min(d for d,i in ss)
-            si = [i for d,i in ss if d == di]
-
-            # now, get the smallest string length
-            ss = [(self.str_len(states[i]), i) for i in si]
-            zi = min(z for z, i in ss)
-            si = [i for z,i in ss if z == zi]
-
-            # now, select all that is different from original constraints
-            si_ = [i for i in si if self.range_at(states[i], last+1) != (m, n)]
-            if si_: si = si_
-
-        else:
-            # not closing. The constraints get priority
-            si = [i for i,s in ss]
-            si_ = [i for i in si if self.range_at(states[i], last+1) != (m, n)]
-            if si_: si = si_
-
-            # then go for the smallest strings
-            ss = [(self.str_len(states[i]), i) for i in si]
-            zi = min(z for z,i in ss)
-            si = [i for z,i in ss if z == zi]
-            # we dont care for stack depth at this point.
-
-        rand_i = random.choice(si)
-        state = states.pop(rand_i)
-        return state, states
+        return pop_random(states)
 
     def choose_a_previous_path(self, states):
-        """
-        Choises: Choose the last state, choose a random state, use a heuristic
-        Heuristic: Rather than go for random state, or the last
-        state, choose the last states with more probability than the first
-        ones.
-        """
-        assert states
-        sl = len(states)
-        arr = []
-        for i in range(sl): arr.extend([i]*(1 + i*Prob_Mul_Factor))
-        si = arr[random.randint(0, len(arr)-1)]
-        state = states[si]
-        states.pop(si)
+        state, states = self.pop_smallest(states)
         return state, states
 
-    def print_constraints(self):
-        for i,c in enumerate(self.state.solver.constraints):
-            v = list(self.state.solver.variables(c))
-            log("\t? %d: %s === %s" % (i, c, str(v)))
+    def constraint_delta(self, s1, s2):
+        # s1 has wider constraints. i.e its min is smaller and max is larger
+        # than s2
+        _min = 0
+        _max = 1
+        c1 = self.get_constraint_range(s1)
+        c2 = self.get_constraint_range(s2)
+        console = False
+        for i, x1 in enumerate(c1):
+            x2 = c2[i] 
+            if not (x1[_min] <= x2[_min], "@%d ! %s <= %s" % (i, str(x1[_min]), str(x2[_min]))):
+                print 'min'
+                console = True
+            if not (x1[_max] >= x2[_max], "@%d ! %s >= %s" % (i, str(x1[_max]), str(x2[_max]))):
+                print 'max'
+                console = True
+            if console:
+                console = False
+                print ""
+                print "parent:", s1, ":", c1
+                print "child:", s2, ":", c2
+                print ""
+                embed(globals(), locals())
 
-    def identical_constraints(self, xs, ys):
-        xsc = claripy.And(*xs)
-        ysc = claripy.And(*ys)
-        return claripy.backends.z3.identical(xsc, ysc)
+        return None, None
 
-    def gc(self, states):
-        if not GC: return states
-        if not Closing_Strip: return states
-        if len(states) > Closing_Buffer:
-            print "start stripping from:", len(states)
-            # strip out all but best 100
-            ss = sorted([(self.stack_depth(s), i) for i, s in enumerate(states)])
-            ss = ss[0:Closing_Buffer]
-            states = [states[i] for d, i in ss]
-            return states
-        return states
+    def get_constraint_change(self, pstate, state):
+        parent_constraint_range = self.get_constraint_range(pstate)
+        pconstraints = pstate.solver.constraints
+        child_constraint_range = self.get_constraint_range(state)
+        _min, _max = 0, 1
+        console = False
+        for i, (p,c) in enumerate(zip(parent_constraint_range, child_constraint_range)):
+            #if not p[_min] <= c[_min]: console = True
+            #if not p[_max] >= c[_max]: console = True
+            pc = pstate.solver.constraints
+            assert pc == pconstraints
+            #if console:
+            #    console = False
+            #    print p, c
+            #    x = self.get_constraint_range(pstate)
+            #    embed(globals(), locals())
+            if p != c: return i # constraints only narrow
+        return None
 
-    def gen_chains(self, state=None):
-        states = self.states if self.states else []
-        if not state: state = self.state
+    def gen_chains(self, state):
+        states = []
+        pstate = None
         while True:
             if self.is_successful(state): return ('success',state)
-            states = self.gc(self.states)
-            try:
-                w("<%d+" % len(states))
-                my_succ = state.step().flat_successors # succ.successors for symbolic
-                nsucc = len(my_succ)
-                w(str(nsucc))
-                w(">")
-                if nsucc == 1:
-                    w(".")
-                    state = my_succ.pop()
-                    self.state = state
-                    continue
-                if nsucc == 0:
-                    # No active successors.
-                    if not states: return ('no_states', None)
-                    log("<< %d(" % len(states))
-                    state, states = self.choose_a_previous_path(states)
-                    self.update_checked_idx()
-                    w("%d)" % self.last_char_checked)
-                    self.state, self.states = state, states
-                elif nsucc > 1:
-                    w("{")
-                    arg = self.get_args(state)
-                    w(repr(arg))
-                    w(",")
-                    state, ss = self.choose_a_successor_state(my_succ)
-                    self.update_checked_idx()
-                    arg = self.get_args(state)
-                    w(repr(arg))
-                    w("}")
-                    states.extend(ss)
-                    self.state, self.states = state, states
-
-                # were there any new chars?
-                w("[")
-                current_constraints = [claripy.simplify(c) for c in state.solver.constraints]
-                if not self.identical_constraints(current_constraints, self.last_constraints):
-                    # TODO: get the last variable checked.
-                    self.last_constraints = current_constraints
-                    # were there any constraints?
-                    if  self.is_printable(self.arg1a[self.last_char_checked+1]):
-                        # log("adding: %s at %d" % (chr(m), self.last_char_checked))
-                        # now concretize
-                        # TODO: save the state with opposite constraints after
-                        # checking unsat
-                        val = state.solver.eval(self.arg1a[self.last_char_checked])
-                        w("@%d: %s" % (self.last_char_checked, chr(val)))
-
-                        # check if an equality operator is involved
-                        c = self.arg1a[self.last_char_checked]
-                        if Quick_Fix and state.solver.max(c) != state.solver.min(c):
-                            not_state = state.copy()
-                            not_state.add_constraints(c != val)
-                            self.extra_states.append(not_state)
-                            state.add_constraints(c == val)
-                        self.update_constraint_rep(state)
-                        log("]")
-                    else:
-                        # the constraint added was not one on the input character
-                        # hence we ignore.
-                        w("x]")
-            except angr.errors.SimUnsatError, ue:
-                log('unsat.. %s' % str(ue))
+            # successors for symbolic
+            flat_successors = state.step().flat_successors
+            my_successors = [(state, s) for s in flat_successors]
+            nsucc = len(my_successors)
+            if nsucc == 1:
+                # constraints will not change here unless ASSERT is there.(TODO)
+                pstate, state = my_successors.pop()
+                # print ">>>>", pstate, state
+                continue
+            elif nsucc == 0:
+                # No active successors.
                 if not states: return ('no_states', None)
-                state, states = self.choose_a_previous_path(states)
-                self.state, self.states = state, states
-                self.update_checked_idx()
+                log("<< %d(" % len(states))
+                (pstate, state), states = self.choose_a_previous_path(states)
+            elif nsucc > 1:
+                (pstate, state), ss = self.choose_a_successor_state(my_successors)
+                states.extend(ss)
+            else: assert False
+            w("%s [%s]" % (repr(self.get_args(pstate)), self.str_len(pstate)))
 
-    def get_constraint_db(self):
-        constraint_db = {}
-        for vi in self.retrieve_vars_in_constraints(self.state):
-            # there could be multiple variables in a constraint
-            # vi is a set of variables.
-            for i in vi:
-                if i in self.arg1h_:
-                    v = self.arg1h_[i]
-                    if v not in constraint_db: constraint_db[v] = 0
-                    constraint_db[v] += 1
-                else:
-                    pass
-                    #log("? %s" % i)
-        return constraint_db
+            # were there any new chars?
+            idx = self.get_constraint_change(pstate, state)
+            if idx is not None:
+                self.extra_states.extend(states)
+                # commit to the state
+                states = []
+                w("\t@%d: %s" % (idx, chr(state.solver.eval(self.arg1a[idx]))))
+            log('')
 
     def get_args(self, state):
         #return state.solver.eval(self.arg1, cast_to=str)[0:self.last_char_checked+1]
@@ -355,36 +251,38 @@ class Program:
 
     def print_args(self, state):
         for i in state.solver.eval_upto(self.arg1, 1, cast_to=str):
-            log(repr(i[0:self.last_char_checked+1])) #.strip('\x00\xff')))
+            log(repr(i)) #.strip('\x00\xff')))
 
 Show_Range = False
 def main(exe):
     prog = Program(exe)
     status, state = None, None
     with open("results.txt", "w+") as f:
+        state = prog.initial_state
         while True:
-            status, state = prog.gen_chains()
+            status, state = prog.gen_chains(state)
             print status
-            prog.print_args(state)
             if status == 'success':
-                prog.update_checked_idx()
+                prog.print_args(state)
+                prog.last_char_checked = prog.last_checked_idx(state)
                 print >>f, "<%s>" % repr(prog.get_args(state))
                 if Show_Range:
                     minval = []
                     maxval = []
                     for i in range(prog.last_char_checked+1):
-                        c = prog.arg1a[i]
-                        x, y = state.solver.min(c), state.solver.max(c)
+                        x, y = prog.range_at(state, i)
                         minval.append(chr(x))
                         maxval.append(chr(y))
                     print >>f, "min:", repr("".join(minval))
                     print >>f, "max:", repr("".join(maxval))
-                # print >>f, "\t (%s)" % repr(i[0:prog.last_char_checked+1])
                 f.flush()
-            log("remaining: %d" % len(prog.states))
-            if not prog.states:
-                print "No more states extra_states:", len(prog.extra_states)
-                break
-            prog.state = prog.states.pop()
-
+                if prog.extra_states:
+                   (pstate, state), prog.extra_states = prog.choose_a_previous_path(prog.extra_states)
+                else:
+                    break
+            else:
+                if prog.extra_states:
+                   (pstate, state), prog.extra_states = prog.choose_a_previous_path(prog.extra_states)
+                else:
+                    break
 main(sys.argv[1])
